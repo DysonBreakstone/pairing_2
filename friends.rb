cb9c63b4933d6d9a87c81e562cddb2032378eff8
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2023-01-04 19:19:36 -0700

    Add new array and new method

--- friends.rb
+++ friends.rb
@@ -1,5 +1,11 @@
 arrays = ["Barbara", "Jeeves", "Samuel", "Harbinger", "Granulum"]
+places = ["Paris", "Vietnam", "Iceland", "Kyoto", "Mongolia"]
+
 
 puts arrays[2]
 puts arrays[4]
 puts arrays[0]
+
+
+places.insert(3, "Puerto Rico", "Antarctica")
+puts places
